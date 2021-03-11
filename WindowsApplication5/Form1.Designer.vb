e2c0c0ff4a7e47896b038b7482f31b7e1f9a37cb
MrCoult <80452894+MrCoult@users.noreply.github.com>
2021-03-11 13:48:35 +0200

    Add files via upload

---
+++ WindowsApplication5/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(107, 33)
+        Me.ProgressBar1.Maximum = 1000000
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(486, 40)
+        Me.ProgressBar1.Step = 1
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(107, 112)
+        Me.ProgressBar2.Maximum = 1000000
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(486, 40)
+        Me.ProgressBar2.Step = 1
+        Me.ProgressBar2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Player 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(36, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Player 2"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 300
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(518, 176)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(605, 211)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Race"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WindowsApplication5/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim varSTART As Boolean
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not varSTART Then
+            ProgressBar1.Value = 0
+            ProgressBar2.Value = 0
+            Timer1.Start()
+            Button1.Text = "Stop"
+            varSTART = False
+        Else
+            Timer1.Stop()
+            Button1.Text = "Start"
+            varSTART = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim varPAS1 As Integer
+        Dim varPAS2 As Integer
+        varPAS1 = Rnd() * 10000
+        ProgressBar1.Increment(varPAS1)
+        varPAS2 = Rnd() * 10000
+        ProgressBar2.Increment(varPAS2)
+        If ProgressBar1.Value >= ProgressBar1.Maximum And ProgressBar1.Value >= ProgressBar1.Maximum Then
+            Timer1.Stop()
+            varSTART = False
+            Button1.Text = "Start"
+            MsgBox("Draw")
+        ElseIf ProgressBar1.Value >= ProgressBar1.Maximum Then
+            Timer1.Stop()
+            varSTART = False
+            Button1.Text = "Start"
+            MsgBox("Player 1 Wins")
+        ElseIf ProgressBar2.Value >= ProgressBar2.Maximum Then
+            Timer1.Stop()
+            varSTART = False
+            Button1.Text = "Start"
+            MsgBox("Player 2 Wins")
+        End If
+    End Sub
+End Class
